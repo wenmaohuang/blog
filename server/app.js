1ca464a5632fa688811e042aad8a6d18266a4bc9
unknown <wenmaohuang@hotmail.com>
2020-04-14 22:41:16 +0800

    9 commit

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cookieParser())
-console.log(path.join(path.join(__dirname, '../client/dist')));
+// console.log(path.join(path.join(__dirname, '../client/dist')));
 app.use(express.static(path.join(__dirname, '../client/dist')))
 app.use(express.static(path.join(__dirname, 'public')));
 app.use((req,res,next)=>{
