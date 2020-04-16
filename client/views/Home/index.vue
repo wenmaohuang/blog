d5ab11d1580f2b6eb1db060d720dba54878aa032
unknown <wenmaohuang@hotmail.com>
2020-04-16 15:38:18 +0800

    9 commit

--- client/views/Home/index.vue
+++ client/views/Home/index.vue
@@ -1,59 +1,13 @@
 <template>
     <div class="main">
-        <div class="first" :style="{height:bgHeight+'px'}">
+        <div class="first" :style="{height:bgHeight+'px',display:'flex',justifyContent:'center',alignItems:'center',}">
+            <a href="http://www.fyyd.vip/#/blog/0" :style="{display:'inlineBlock',fontSize:'40px',color:'#f00',textDecoration:'none'}">ENTER BLOG</a>
+
         </div>
         <div class="second">
-            <ul>
-                <li>
-                    <a href="https://github.com/afeifeifei/front-end-interview-handbook/blob/master/Translations/Chinese/README.md"
-                       :style="{display:'block',textDecoration:'none',textAlign:'center'}">面试资料</a>
-                </li>
-                <li>
-                    <a href="http://localhost/#/blog/0" :style="{display:'block',textDecoration:'none',textAlign:'center'}">本地服务器80</a>
-                </li>
-                <li>
-                    <a href="http://localhost:8080/#/blog/0" :style="{display:'block',textDecoration:'none',textAlign:'center'}">本地服务器8080</a>
-                </li>
-                <li>
-                    <a href="https://github.com/" :style="{display:'block',textDecoration:'none',textAlign:'center'}">GITHUB</a>
-                </li>
-                <li>
-                    <a href="http://www.fyyd.vip/#/blog/0" :style="{display:'block',textDecoration:'none',textAlign:'center'}">express服务器</a>
-                </li>
-                <li>
-                    <a href="http://www.fyyd.vip:3000" :style="{display:'block',textDecoration:'none',textAlign:'center'}">nginx服务器</a>
-                </li>
-                <li>
-                    <a href="https://www.yanshisan.cn/Blog/Article" :style="{display:'block',textDecoration:'none',textAlign:'center'}">十三燕</a>
-                </li>
-                <li>
-                    <a href="https://y.qq.com/portal/player.html" :style="{display:'block',textDecoration:'none',textAlign:'center'}">QQ音乐</a>
-                </li>
-                <li>
-                    <a href="https://www.aliyun.com/" :style="{display:'block',textDecoration:'none',textAlign:'center'}">阿里云</a>
-                </li>
-
-                <li>
-                    <a href="https://www.umei.cc/bizhitupian/meinvbizhi/" :style="{display:'block',textDecoration:'none',textAlign:'center'}">美女图片</a>
-                </li>
-                <li>
-                    <a href="https://www.shiguangkey.com/video/5404?classId=13017&videoId=221997"
-                       :style="{display:'block',textDecoration:'none',textAlign:'center'}">潭州教育</a>
-                </li>
-                <li>
-                    <a href="https://fanyi.baidu.com/?aldtype=16047#auto/zh"
-                       :style="{display:'block',textDecoration:'none',textAlign:'center'}">百度翻译</a>
-                </li>
-                <li>
-                    <a href="http://www.mongoosejs.net/docs/models.html"
-                       :style="{display:'block',textDecoration:'none',textAlign:'center'}">mongoose数据库</a>
-                </li>
-                <li>
-                    <a href="http://baidu.com" :style="{display:'block',textDecoration:'none',textAlign:'center'}">百度搜素</a>
-                </li>
-                <li>
-                    <a href="https://element.eleme.cn/#/zh-CN/component/installation"
-                       :style="{display:'block',textDecoration:'none',textAlign:'center'}">elementUI组件</a>
+            <ul >
+                <li v-for="(i,index) in obj.href" :key="index">
+                    <a :href="i" :style="style">{{obj.msg[index]}}</a>
                 </li>
             </ul>
         </div>
@@ -66,16 +20,32 @@
         data() {
             return {
                 isCollapse: true,
-                bgHeight:0
+                bgHeight:0,
+                style:{display:'block',textDecoration:'none',textAlign:'center'},
+                obj:{
+                    msg:['面试资料','本地server','本地client','GITHUB',
+                        'express服务器','nginx服务器','十三燕','QQ音乐','阿里云',
+                        '美女图片','潭州教育','百度翻译','mongoose数据库','百度搜素','elementUI组件'],
+                    href:['https://github.com/afeifeifei/front-end-interview-handbook/blob/master/Translations/Chinese/README.md',
+                    'http://localhost/#/blog/0',
+                    'http://localhost:8080/#/blog/0',
+                        'https://github.com/',
+                        'http://www.fyyd.vip/#/blog/0',
+                        'http://www.fyyd.vip:3000',
+                        'https://www.yanshisan.cn/Blog/Article',
+                        'https://y.qq.com/portal/player.html',
+                        'https://www.aliyun.com/',
+                        'https://www.umei.cc/bizhitupian/meinvbizhi/',
+                        'https://www.shiguangkey.com/video/5404?classId=13017&videoId=221997',
+                        'https://fanyi.baidu.com/?aldtype=16047#auto/zh',
+                        'http://www.mongoosejs.net/docs/models.html',
+                        'http://baidu.com',
+                        'https://element.eleme.cn/#/zh-CN/component/installation'
+                    ]
+                }
             };
         },
         methods: {
-            handleOpen(key, keyPath) {
-                console.log(key, keyPath);
-            },
-            handleClose(key, keyPath) {
-                console.log(key, keyPath);
-            },
             getWindowHeight(){
                 this.bgHeight = window.innerHeight
             }
@@ -96,7 +66,6 @@
         .first{
             background-image: url("../../assets/img/3.jpg");
             background-size:100% 100%;
-
         }
         .second{
             ul{
