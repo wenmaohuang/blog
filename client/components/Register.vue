414a1198622905c4004f297b2236741a2d7ac9db
unknown <wenmaohuang@hotmail.com>
2020-04-26 23:33:40 +0800

    1

--- client/components/Register.vue
+++ client/components/Register.vue
@@ -1,58 +1,110 @@
 <template>
-    <div class="register">
-        <h3>register</h3>
-        <el-form ref="form" :model="form" label-width="80px" :rules="rules" class="form">
-            <el-form-item label="用户名" prop="user">
-                <el-input v-model="form.user"></el-input>
-            </el-form-item>
-            <el-form-item label="密码">
-                <el-input v-model="form.pwd" show-password></el-input>
-            </el-form-item>
-            <el-form-item label="确认密码">
-                <el-input v-model="form.checkpwd" show-password></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-button type="success">立即注册</el-button>
-            </el-form-item>
-        </el-form>
-    </div>
+  <div class="register">
+    <!-- <h3>register</h3> -->
+    <el-form ref="form" :model="form" label-width="80px" :rules="rules" class="form">
+      <el-form-item label="用户名" prop="user">
+        <el-input v-model="form.user"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="pwd">
+        <el-input v-model="form.pwd" show-password></el-input>
+      </el-form-item>
+      <el-form-item label="确认密码" prop="checkpwd">
+        <el-input v-model="form.checkpwd" show-password></el-input>
+      </el-form-item>
+       <el-form-item label="验证码" prop="vcode">
+        <el-input v-model="form.vcode"></el-input>
+        <div class="svg">svg</div>
+        <div class="refresh">刷新</div>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="success">立即注册</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Register",
-        data(){
-           return{
-               form:{
-                   user:'',
-                   pwd:'',
-                   checkpwd:''
-               },
-               rules:{
-                    user:[
-                        { required: true, message: '请输入用户名', trigger: 'blur' },
-                        {type:"string", min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
-                    ],
+export default {
+  name: "Register",
+  data() {
+    return {
+      form: {
+        user: "",
+        pwd: "",
+        checkpwd: ""
+      },
+      rules: {
+        user: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          {
+            type: "string",
+            min: 3,
+            max: 8,
+            message: "长度在 3 到 8 个字符",
+            trigger: "blur"
+          }
+        ],
+        pwd: {
+          type: "string",
+          validator:(rule,value,cb)=>{
+              if (value) {
+                  if (/^[\w<>,.?|;':"{}!@#$%^&*()\-\\]{6,12}$/.test(value)) {
+                      cb()
+                  } else {
+                      cb(new Error("长度在 6 到 12 个字符"))
+                  }
+              } else {
+                cb(new Error("请输入密码"))
+              }
+              this.form.checkpwd && this.$refs.form.validateField("checkpwd")
 
-               }
-           }
+              console.log(this.form.checkpwd)
+          },
+        //   min: 6,
+        //   max: 12,
+          required: true,
+        //   message: "长度在 6 到 12 个字符",
+          trigger: ["blur","change"]
         },
-        mounted(){
-            // console.log(this,'555')
-        },
-        methods:{
-            
+        checkpwd: {
+          validator: (rule, value, cb) => {
+            if (value) {
+              if (value === this.form.pwd) {
+                cb();
+              } else {
+                cb(new Error("两次密码不一样"));
+              }
+            } else {
+              cb(new Error("请再次输入密码"));
+            }
+          },
+          trigger: ["blur","change"]
+
         }
-    }
+      }
+    };
+  },
+  mounted() {
+    // console.log(this,'555')
+  },
+  methods: {}
+};
 </script>
 
 <style lang="less" scoped>
-    .register{
-        /*background-color: #555;*/
-        el-form{
-            /*background-color: #aaa;*/
-            
-        }
+.register {
+  /*background-color: #555;*/
+  el-form {
+    /*background-color: #aaa;*/
+    .vcode{
+      .el-input{
+        float:left
+        
+      }
+      div.svg{
+        float:left
+      }
     }
-
+  }
+}
 </style>
--- client/views/Home/index.vue
+++ client/views/Home/index.vue
@@ -25,7 +25,7 @@
                 obj:{
                     msg:['面试资料','本地server','本地client','GITHUB',
                         'express服务器','nginx服务器','十三燕','QQ音乐','阿里云',
-                        '美女图片','潭州教育','百度翻译','mongoose数据库','百度搜素','elementUI组件','cctv'],
+                        '美女图片','潭州教育','百度翻译','mongoose数据库','百度搜素','elementUI组件','cctv','潭州教育remi'],
                     href:['https://github.com/afeifeifei/front-end-interview-handbook/blob/master/Translations/Chinese/README.md',
                     'http://localhost/#/blog/0',
                     'http://localhost:8080/#/blog/0',
@@ -41,7 +41,8 @@
                         'http://www.mongoosejs.net/docs/models.html',
                         'http://baidu.com',
                         'https://element.eleme.cn/#/zh-CN/component/installation',
-                        'http://tv.cctv.com/live/'
+                        'http://tv.cctv.com/live/',
+                        'https://www.shiguangkey.com/video/903?videoId=242629&classId=17830&playback=1'
                     ]
                 }
             };
