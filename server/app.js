01e7935ec533e7450ab13a19f72e4ffe8fa57bc6
unknown <wenmaohuang@hotmail.com>
2020-05-13 00:40:54 +0800

    1

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, '../client/dist')))
-//app.use(express.static(path.join(__dirname, './public')))
+// app.use(express.static(path.join(__dirname, './public')))
 
 app.use((req,res,next)=>{
     res.header({
