a468918fe030ac23775c596b99ead08a178eab9e
wen <wenmaohuang@hotmail.com>
2020-07-13 13:56:43 +0800

    1

--- server/bin/www.js
+++ server/bin/www.js
@@ -3,21 +3,23 @@ var debug = require('debug')('server:server')
 var fs = require('fs')
 var path = require('path')
 var http = require('http')
-var https = require('https')
-var options = {
-    // key:fs.readFileSync(path.join(__dirname,'../https/privatekey.pem')),
-    // cert:fs.readFileSync(path.join(__dirname,'../https/certificate.pem')),
-    key:fs.readFileSync(path.join(__dirname,'../https/4197482_www.fyyd.vip.key')),
-    cert:fs.readFileSync(path.join(__dirname,'../https/4197482_www.fyyd.vip.pem'))
-}
-var port = normalizePort(process.env.PORT || '443')
+// var https = require('https')
+// var options = {
+//     // key:fs.readFileSync(path.join(__dirname,'../https/privatekey.pem')),
+//     // cert:fs.readFileSync(path.join(__dirname,'../https/certificate.pem')),
+//     key:fs.readFileSync(path.join(__dirname,'../https/4197482_www.fyyd.vip.key')),
+//     cert:fs.readFileSync(path.join(__dirname,'../https/4197482_www.fyyd.vip.pem'))
+// }
+var port = normalizePort(process.env.PORT || '80')
 app.set('port', port)
-http.createServer(app).listen(80)
-var server = https.createServer(app)
-https.createServer(options,app)
+// http.createServer(app).listen(80)
+var server = http.createServer(app)
+// https.createServer(options,app)
 server.listen(port)
 server.on('error', onError)
 server.on('listening', onListening)
+var host = server.address().address;
+console.log('Example app listening at http://%s:%s', host, port);
 
 function normalizePort(val) {
     var port = parseInt(val, 10)
