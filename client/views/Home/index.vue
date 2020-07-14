812bd517aa23eaa81353b62e7a0bad9634e4ad0f
wen <wenmaohuang@hotmail.com>
2020-07-14 20:22:14 +0800

    1

--- client/views/Home/index.vue
+++ client/views/Home/index.vue
@@ -139,7 +139,8 @@ export default {
                 xiaochengxu:"https://developers.weixin.qq.com/miniprogram/en/dev/framework/",
                 xiaochengxuAdmin:"https://mp.weixin.qq.com/wxamp/wacodepage/getcodepage?token=174094833&lang=zh_CN",
                 bilibili:"https://www.bilibili.com/video/BV1nE41117BQ?p=9",
-                move:"http://27k.cc/?m=vod-play-id-37805-src-1-num-1.html"
+                move:"http://27k.cc/?m=vod-play-id-37805-src-1-num-1.html",
+                flutter:"https://flutter.dev/docs/development/tools/android-studio"
             }
         };
     },
