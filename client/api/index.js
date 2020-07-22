09b9e735472defaa3d7ecd97768f3806a70390d1
wen <wenmaohuang@hotmail.com>
2020-07-22 15:48:10 +0800

    1

--- client/api/index.js
+++ client/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-// axios.defaults.baseURL = 'http://localhost:3000'
-axios.defaults.baseURL = 'https://www.fyyd.vip'
+axios.defaults.baseURL = 'http://localhost:3000'
+// axios.defaults.baseURL = 'https://www.fyyd.vip'
 
 
 
