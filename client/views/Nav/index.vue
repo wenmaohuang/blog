cb817129b11962d5756d471e50eb81bc9776bb01
unknown <wenmaohuang@hotmail.com>
2020-09-08 19:04:55 +0800

    '1'

--- client/views/Nav/index.vue
+++ client/views/Nav/index.vue
@@ -297,7 +297,7 @@ export default {
       if (res.data.userInfo) {
         this.ifLogin = true;
         this.login.user = res.data.userInfo.user;
-        this.login.photo = "http://www.fyyd.vip" + res.data.userInfo.photo;
+        this.login.photo = "https://www.fyyd.vip" + res.data.userInfo.photo;
       } else {
         this.ifLogin = false;
       }
