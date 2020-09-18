2652d518b3af616891c1a741c881daed6758f83f
unknown <wenmaohuang@hotmail.com>
2020-09-19 02:42:20 +0800

    '1'

--- client/api/index.js
+++ client/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 // axios.defaults.baseURL = 'http://localhost:3000'
-axios.defaults.baseURL = 'http://www.fyyd.vip'
+axios.defaults.baseURL = 'https://www.fyyd.vip'
 
 // const instance = axios.create({
 //     baseURL: process.env.BASE_URL
