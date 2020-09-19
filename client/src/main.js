908540b03edcd72a05ee74e32493b992e62ce548
unknown <wenmaohuang@hotmail.com>
2020-09-19 21:43:18 +0800

    '1'

--- client/src/main.js
+++ client/src/main.js
@@ -22,6 +22,18 @@ if (process.env.NODE_ENV === 'development') {
 
 Vue.use(VueWechatTitle)
 
+// import Vue from 'vue'
+import VueVideoPlayer from 'vue-video-player'
+
+// require videojs style
+import 'video.js/dist/video-js.css'
+// import 'vue-video-player/src/custom-theme.css'
+
+Vue.use(VueVideoPlayer, /* {
+  options: global default options,
+  events: global videojs events
+} */)
+
 // Vue.use(ELEMENT)
 
 
--- client/views/Football/Football.vue
+++ client/views/Football/Football.vue
@@ -1,20 +1,29 @@
 <template>
-  <div>
-    <a href="http://www.azhibo.com/zuqiuzhibo">足球直播</a>
+  <div class="main">
+    <div style="margin: 10px;">
+      <a href="http://www.azhibo.com/zuqiuzhibo" style="font-size: 20px;padding: 10px;">足球直播</a>
+      <div style="width:700px;height: 400px;margin-top: 10px">
+        <Video></Video>
+      </div>
+    </div>
     <Websocket></Websocket>
   </div>
 </template>
 
 <script>
 import Websocket from "@/components/Websocket";
+import Video from "@/components/Video";
 export default {
   name: "football",
   components:{
-    Websocket
+    Websocket,
+    Video
   }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+.main{
+  display: flex;
+}
 </style>
