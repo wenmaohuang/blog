f5d6c92b87959e187110fc10aa2e4afd38bbb138
unknown <wenmaohuang@hotmail.com>
2020-09-20 11:13:26 +0800

    '1'

--- client/views/Football/Football.vue
+++ client/views/Football/Football.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="main">
     <div style="margin: 10px;">
-      <a href="http://www.azhibo.com/zuqiuzhibo" style="font-size: 20px;padding: 10px;">足球直播</a>
+      <a href="http://www.azhibo.com/zuqiuzhibo" style="font-size: 20px;padding: 10px;">A直播</a>
+      <a href="https://kanqiubao.com/room/10716/" style="font-size: 20px;padding: 10px;">看球宝</a>
+
       <div style="width:700px;height: 400px;margin-top: 10px">
         <Video></Video>
       </div>
