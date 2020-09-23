974d2b45e34c5c43579cff7e986c2364d84abcef
unknown <wenmaohuang@hotmail.com>
2020-09-23 21:46:41 +0800

    '1'

--- client/src/components/Video.vue
+++ client/src/components/Video.vue
@@ -1,44 +1,59 @@
 <template>
-  <div class="dialog-body-content-base-style" style="width: 100%;max-width:100%;height:0">
-    <video-player class="video-player vjs-custom-skin" style="height:200px" ref="positiveVideoPlayer" :playsinline="true" :options="positivePlayerOptions"></video-player>
+  <div>
+    <video id="videoElement"  class="flvplayer-app"></video>
 
+    <button @click="play">
+      播放
+    </button>
+    <button @click="pause">
+      暂停
+    </button>
   </div>
-</template>
-<script>
-import {videoPlayer} from 'vue-video-player';
-import 'video.js/dist/video-js.css'
 
+</template>
+
+<script>
+
+// import flvPlayer  from 'flvplayer'
+import flvPlayer from 'flv.js'
 export default {
-  name: 'proviewAttachment',
-  components: {
-    videoPlayer
-  },
-  data() {
+  name: 'flv',
+  data () {
     return {
-      positivePlayerOptions: {
-        playbackRates: [0.5, 1.0, 1.5, 2.0], // 播放速度
-        autoplay: false, // 如果true,浏览器准备好时开始回放。
-        muted: false, // 默认情况下将会消除任何音频。
-        loop: false, // 导致视频一结束就重新开始。
-        preload: 'auto', // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
-        language: 'zh-CN',
-        aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
-        fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
-        sources: [{
-          type: '',
-          src: 'https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm' // 视频url地址
-        }],
-        poster: '', // 你的封面地址
-        // width: document.documentElement.clientWidth,
-        notSupportedMessage: '此视频暂无法播放，请稍后再试', // 允许覆盖Video.js无法播放媒体源时显示的默认信息。
-        controlBar: {
-          timeDivider: true,
-          durationDisplay: true,
-          remainingTimeDisplay: false,
-          fullscreenToggle: true // 全屏按钮
-        }
-      }
+      flvPlayer:null,
+    }
+  },
+  props:{
+    videoSrc:{
+      type: String,
+
+      default: 'http://zb.fyyd.vip/app/stream.flv?auth_key=1600868383-0-0-e2949e19ba55d216b94dc43cd80b9683'
+    }
+  },
+  mounted () {
+    if(flvPlayer.isSupported()){
+      var videoElement = document.getElementById('videoElement')
+      var videoSrc = this.videoSrc
+      this.flvPlayer = flvPlayer.createPlayer({
+        // url:"http://cyberplayerplay.kaywang.cn/cyberplayer/demo201711-L1.flv",
+        url:videoSrc,
+        type: 'flv',
+      })
+      this.flvPlayer.attachMediaElement(videoElement)
+      this.flvPlayer.load()
+    }
+  },
+  methods:{
+    play(){
+      this.flvPlayer.play();
+    },
+    pause(){
+      this.flvPlayer.pause();
     }
   }
 }
 </script>
+
+<style lang="less" scoped>
+
+</style>
