fcee225109ca4bd12700c694c8f0896d8ab103b0
风移影动 <oncwnuFE28RNcGrpLh3eWbhn5qM4@git.weixin.qq.com>
2020-10-20 18:11:28 +0800

    '1'

--- client/config/index.js
+++ client/config/index.js
@@ -12,7 +12,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/',
     proxyTable: {
 
      
@@ -60,7 +60,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
     url:'https://www.fyyd.vip',
     /**
      * Source Maps
