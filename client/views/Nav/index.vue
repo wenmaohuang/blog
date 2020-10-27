4b17089e1a1f3f79fdc4ca5ab7ddffbe1e590417
风移影动 <oncwnuFE28RNcGrpLh3eWbhn5qM4@git.weixin.qq.com>
2020-10-28 01:27:46 +0800

    '1'

--- client/views/Nav/index.vue
+++ client/views/Nav/index.vue
@@ -132,7 +132,15 @@ export default {
     handldeLogin(data, opts) {
       if (QC.Login.check()) {
         // this.$store.state.ifLogin = false;
-        this.$store.state.showLogin = false;
+
+        if(QC.Login.signOut()){
+          this.$store.state.showLogin = true;
+
+        }else {
+          this.$store.state.showLogin = false;
+
+        }
+
         // console.log(this.$store.state.ifLogin, 'vb')
       } else {
         this.$store.state.showLogin = true;
