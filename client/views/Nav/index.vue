5b2b23eeb5d0d67fee15d882845493845987a52a
风移影动 <oncwnuFE28RNcGrpLh3eWbhn5qM4@git.weixin.qq.com>
2020-10-27 17:54:46 +0800

    '1'

--- client/views/Nav/index.vue
+++ client/views/Nav/index.vue
@@ -124,29 +124,7 @@ export default {
       window.location.reload();
     });
 
-    postIfLogin().then((res) => {
-      console.log(res.data.userInfo, 'cv')
-      if (res.data.userInfo) {
-        this.$store.state.ifLogin = true;
-        this.$store.state.mongoLogin = true;
-        this.$store.state.showQQLogin = false;
 
-
-        this.$store.state.userInfo = res.data.userInfo;
-
-        console.log(this.$store.state.ifLogin, 'zx')
-
-        this.login.user = res.data.userInfo.user;
-        this.login.photo = "https://www.fyyd.vip/blog" + res.data.userInfo.photo;
-      } else {
-        // this.ifLogin = false;
-        this.$store.state.ifLogin = false;
-        this.$store.state.showQQLogin = true;
-
-        console.log(this.$store.state.ifLogin, 'xc')
-
-      }
-    });
 
   },
   components: {
@@ -349,7 +327,29 @@ export default {
   },
   created() {
 
+    postIfLogin().then((res) => {
+      console.log(res.data.userInfo, 'cv')
+      if (res.data.userInfo) {
+        this.$store.state.ifLogin = true;
+        this.$store.state.mongoLogin = true;
+        this.$store.state.showQQLogin = false;
 
+
+        this.$store.state.userInfo = res.data.userInfo;
+
+        console.log(this.$store.state.ifLogin, 'zx')
+
+        this.login.user = res.data.userInfo.user;
+        this.login.photo = "https://www.fyyd.vip/blog" + res.data.userInfo.photo;
+      } else {
+        // this.ifLogin = false;
+        this.$store.state.ifLogin = false;
+        this.$store.state.showQQLogin = true;
+
+        console.log(this.$store.state.ifLogin, 'xc')
+
+      }
+    });
 
   },
 };
