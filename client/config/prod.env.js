354725e5ba628d9dfc76b22b6e00c4276b91a8fd
unknown <wenmaohuang@hotmail.com>
2020-11-17 22:43:51 +0800

    '1'

--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -1,7 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  // BASE_URL:'"https://www.fyyd.vip"',
-  BASE_URL:'"http://localhost:3000"'
+  BASE_URL:'"https://www.fyyd.vip"',
 
 }
